05c56a97d90ec8983d700cb799f3a242a1c13df7
karim wagdi <k.wagdi17@gmail.com>
2023-11-18 13:35:28 +0200

    departmentModel

---
+++ src/Controller/DepartmentController.ts
@@ -0,0 +1,90 @@
+import { Request, Response } from 'express'
+import { PrismaClient } from '@prisma/client'
+import { sendResponse } from '../helper/sendResponse'
+const prisma = new PrismaClient()
+
+class Department {
+    public static createDepartment = async (
+        request: Request,
+        response: Response
+    ) => {
+        try {
+            const {name} = request.body
+            const department = await prisma.department.create({
+                data : {
+                    name:name
+                }
+            });
+            return sendResponse(response, 200, 'success', department)
+        } catch (err : any){
+            return sendResponse(response, 404, "error can't create department.", err)
+        }
+    }
+    
+    public static getAllDepartments = async (
+        request: Request,
+        response: Response 
+    ) => {
+        try {
+            const departments = await prisma.department.findMany()
+            return sendResponse(response, 200, "success", departments)
+        } catch (err : any) {
+            return sendResponse(response, 404, "error can't get departments.", err)
+        }
+    }
+
+    public static getDepartmentById = async (
+        request : Request,
+        response : Response
+    ) => {
+        try {
+            const department = await prisma.department.findUnique({
+                where : {
+                    id: request.params.id
+                }
+            })
+            return sendResponse(response, 200, "success", department)
+        } catch (err : any) {
+            return sendResponse(response, 404, "error can't get department invalid id.", err)
+        }
+    }
+
+    public static updateDepartment = async (
+        request: Request,
+        response: Response
+    ) => {
+        try {
+            const {id, name} = request.body
+            const department = prisma.department.update({
+                data : {
+                    name : name
+                },
+                where : {
+                    id : id
+                }
+            })
+            return sendResponse(response, 200, "success", department)
+        } catch (err : any) {
+            return sendResponse(response, 404, "error can't update department.", err)
+        }
+    }
+
+    public static deleteDepartment = async (
+        request : Request,
+        response : Response
+    ) => {
+        try {
+            const {id} = request.body
+            const department = prisma.department.delete({
+                where: {
+                    id : id
+                }
+            })
+            return sendResponse(response, 200, "success", department)
+        } catch (err : any) {
+            return sendResponse(response, 404, "error can't delete department.", err)
+        }
+    }
+}
+
+export default Department
