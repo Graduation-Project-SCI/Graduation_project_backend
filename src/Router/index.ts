95252f54b83893499d744a13bc84a974c0088eb8
yousef alaa <yousefalaa2279@gmail.com>
2023-11-22 01:05:35 +0200

    update index router

--- src/Router/index.ts
+++ src/Router/index.ts
@@ -1,11 +1,13 @@
 import DoctorRoute from "./DoctorRuote"
 import DepartmentRoute from "./DepartmentRoute"
+import ResearchRoute from "./ResearchRoute"
 import  { Router } from "express";
 
 const routes = Router()
 
 routes.use('/auth', DoctorRoute)
 routes.use('/department', DepartmentRoute)
+routes.use('/research', ResearchRoute)
 
 
 export default routes
