4d8584c68b5d9c5150f94dc4621841f8cbebc028
MoOsman5 <mohamed1592000gmail.com>
2023-11-21 19:34:32 +0200

    add professor ruotes

---
+++ src/Router/ProfessorRuote.ts
@@ -0,0 +1,15 @@
+import { Router } from 'express'
+import AuthController from '../Controller/AuthController'
+import ProfessorController from '../Controller/ProfessorController'
+const router = Router()
+
+router.post('/login', AuthController.login)
+router.post('/createUser',AuthController.createUser)
+router.get('/allProfessors', ProfessorController.getAllProfessors)
+router.get('/Professor/:id', ProfessorController.getProfessorById)
+router.put('/update/:id', ProfessorController.updateProfessor)
+router.delete('/delete/:id', ProfessorController.deleteProfessor)
+
+
+export default router
+
