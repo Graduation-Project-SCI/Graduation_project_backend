dde11a950aa3b1666fbb8b13d9eef2977a31c68b
MoOsman5 <mohamed1592000gmail.com>
2023-11-22 01:45:07 +0200

    create ProfessorAttachment routes

--- src/Router/index.ts
+++ src/Router/index.ts
@@ -5,6 +5,7 @@ import GenderRoute from "./GenderRoute"
 import TypeRoute from "./TypeRoute"
 import StudentRoute from "./StudentRoute"
 import ProfessorRoleRoute from "./ProfessorRoleRoute"
+import ProfessorAttachmentRoute from './ProfessorAttachmentRoute'
 import  { Router } from "express";
 
 const routes = Router()
@@ -17,6 +18,8 @@ routes.use('/gender',GenderRoute)
 routes.use('/type',TypeRoute)
 routes.use('/student',StudentRoute)
 routes.use('/professorRole',ProfessorRoleRoute)
+routes.use('/professorAttachment',ProfessorAttachmentRoute)
+
 
 
 
