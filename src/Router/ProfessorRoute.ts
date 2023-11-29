06977559bf79fea2fae3097c1f22de4b7cb5ad39
yousef alaa <yousefalaa2279@gmail.com>
2023-11-29 02:41:57 +0200

    add auth to professor

--- src/Router/ProfessorRoute.ts
+++ src/Router/ProfessorRoute.ts
@@ -1,14 +1,16 @@
 import { Router } from 'express'
 import AuthController from '../Controller/AuthController'
 import ProfessorController from '../Controller/ProfessorController'
+import tokenValidateMiddleware from '../Auth middleware/authMiddleware'
 const router = Router()
+const tvm = tokenValidateMiddleware
 
 router.post('/login', AuthController.login)
-router.post('/createUser',AuthController.createUser)
-router.get('/allProfessors', ProfessorController.getAllProfessors)
-router.get('/:id', ProfessorController.getProfessorById)
-router.put('/update/:id', ProfessorController.updateProfessor)
-router.delete('/delete/:id', ProfessorController.deleteProfessor)
+router.post('/createUser', AuthController.createUser)
+router.get('/allProfessors', tvm, ProfessorController.getAllProfessors)
+router.get('/:id', tvm, ProfessorController.getProfessorById)
+router.put('/update/:id', tvm, ProfessorController.updateProfessor)
+router.delete('/delete/:id', tvm, ProfessorController.deleteProfessor)
 
 
 export default router
