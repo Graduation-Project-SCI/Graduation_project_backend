d7edb78916198cd6a48e8fcf01a8f7329004708a
MoOsman5 <mohamed1592000gmail.com>
2023-11-30 02:05:38 +0200

    add project route

--- src/Router/index.ts
+++ src/Router/index.ts
@@ -2,6 +2,7 @@ import ProfessorRoute from "./ProfessorRoute"
 import DepartmentRoute from "./DepartmentRoute"
 import ResearchRoute from "./ResearchRoute"
 import StudentRoute from "./StudentRoute"
+import ProjectRoute from "./ProjectRoute"
 import ProfessorAttachmentRoute from './ProfessorAttachmentRoute'
 import AuthRoute from "./AuthRoute"
 import  { Router } from "express";
@@ -10,6 +11,7 @@ const routes = Router()
 
 routes.use('/auth', AuthRoute)
 routes.use('/professor', ProfessorRoute)
+routes.use('/project', ProjectRoute)
 routes.use('/department', DepartmentRoute)
 routes.use('/research', ResearchRoute)
 routes.use('/student',StudentRoute)
