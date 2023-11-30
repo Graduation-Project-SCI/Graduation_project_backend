d25dd9064ec272808969d45a2edb548bb87d71c9
MoOsman5 <mohamed1592000gmail.com>
2023-11-30 02:04:58 +0200

    edit project crud

--- src/Controller/ProjectController.ts
+++ src/Controller/ProjectController.ts
@@ -1,67 +1,76 @@
-import { PrismaClient } from '@prisma/client';
-import { Request, Response } from 'express'; // Import Request and Response from Express
+import { PrismaClient } from "@prisma/client";
+import { Request, Response } from "express"; // Import Request and Response from Express
+import { sendResponse } from "../helper/sendResponse";
 
+const prisma = new PrismaClient();
 class ProjectController {
-  private prisma = new PrismaClient();
-
   // Create a new project
-  async createProject(request: Request, response: Response): Promise<Response> {
+  public static async createProject(
+    request: Request,
+    response: Response
+  ) {
     try {
       const data = request.body; // Assuming data is in the request body
-      const newProject = await this.prisma.project.create({ data });
-      return response.status(201).json(newProject);
-    } catch (error) {
-      return response.status(500).json({ message: error.message });
+      const newProject = await prisma.project.create({ data });
+      return sendResponse(response, 200, "success", newProject);
+    } catch (err: unknown) {
+      return sendResponse(response, 404, "error can't create Project.", err);
     }
   }
 
   // Get a single project by ID
-  async getProjectById(request: Request, response: Response): Promise<Response> {
+  public static async getProjectById(
+    request: Request,
+    response: Response
+  ): Promise<Response> {
     try {
       const projectId = parseInt(request.params.projectId); // Assuming projectId is a route parameter
-      const project = await this.prisma.project.findUnique({
+      const project = await prisma.project.findUnique({
         where: { projectId },
       });
       if (!project) {
-        return response.status(404).json({ message: 'Project not found' });
+        return response.status(404).json({ message: "Project not found" });
       }
-      return response.json(project);
-    } catch (error) {
-      return response.status(500).json({ message: error.message });
+      return sendResponse(response, 200, "success", project);
+    } catch (err: unknown) {
+      return sendResponse(response, 404, "error can't get Project.", err);
     }
   }
 
-  async getAllProjects(request: Request, response: Response): Promise<Response> {
+  public static async getAllProjects(
+    request: Request,
+    response: Response
+  ): Promise<Response> {
     try {
-      const projects = await this.prisma.project.findMany();
-      return response.json(projects);
-    } catch (error) {
-      return response.status(500).json({ message: error.message });
+      const projects = await prisma.project.findMany();
+      return sendResponse(response, 200, "success", projects);
+    } catch (err: unknown) {
+      return sendResponse(response, 404, "error can't get Projects.", err);
     }
   }
 
   // Update a project by ID
-  async updateProject(request: Request, response: Response): Promise<Response> {
+  public static async updateProject(request: Request, response: Response): Promise<Response> {
     try {
       const projectId = parseInt(request.params.projectId); // Assuming projectId is a route parameter
       const data = request.body; // Assuming updated data is in the request body
-      const updatedProject = await this.prisma.project.update({
+      const updatedProject = await prisma.project.update({
         where: { projectId },
         data,
       });
-      return response.json(updatedProject);
-    } catch (error) {
-      return response.status(500).json({ message: error.message });
+      return sendResponse(response, 200, "success", updatedProject);
+    } catch (err: unknown) {
+      return sendResponse(response, 404, "error can't update Project.", err);
     }
   }
 
-  async deleteProject(request: Request, response: Response): Promise<Response> {
+  public static async deleteProject(request: Request, response: Response): Promise<Response> {
     try {
       const projectId = parseInt(request.params.projectId); // Assuming projectId is a route parameter
-      await this.prisma.project.delete({ where: { projectId } });
-      return response.status(200).json({ message: 'Project successfully deleted' });
-    } catch (error) {
-      return response.status(500).json({ message: error.message });
+      await prisma.project.delete({ where: { projectId } });
+      return sendResponse(response, 200, "success", projectId);
+    } catch (err: unknown) {
+      return sendResponse(response, 404, "error can't delet Project.", err);
     }
   }
 }
